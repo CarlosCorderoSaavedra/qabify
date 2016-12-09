0fccf10d323601143ea8898477cde43fa82fb348
Carlos Lamb <thebiggcaok@gmail.com>
2016-12-09 17:51:59 +0100

    add show password test
    Adding show password test and code refactor

--- src/test/java/appium/tutorial/android/AutomatingASimpleActionTest.java
+++ src/test/java/appium/tutorial/android/AutomatingASimpleActionTest.java
@@ -16,26 +16,37 @@ public class AutomatingASimpleActionTest extends AppiumTest {
 	String passwordButtonId = "com.cabify.qabify:id/password";
 	String loginButtonId = "com.cabify.qabify:id/email_sign_in_button";
 	String showPasswordId = "com.cabify.qabify:id/text_input_password_toggle";
-    @org.junit.Test
-    public void checkUserOne() throws Exception {
+    
+	@org.junit.Test
+    public void checkUserOneTest() throws Exception {
     	
-    	
-    //	driver.findElementById(mailButtonId).click(); 
+    	 
         driver.findElementById(mailButtonId).sendKeys("user1@example.com");
-    //    driver.findElementById(passwordButtonId).click();
         driver.findElementById(passwordButtonId).sendKeys("cGFzc3dvcmQ=");
         driver.findElementById(loginButtonId).click();
         
     }
-    public void checkUserAdmin() throws Exception {
+	
+	@org.junit.Test
+    public void checkUserAdminTest() throws Exception {
     	
     	
-        //	driver.findElementById(mailButtonId).click(); 
+      
             driver.findElementById(mailButtonId).sendKeys("admin@example.com");
-        //    driver.findElementById(passwordButtonId).click();
             driver.findElementById(passwordButtonId).sendKeys("bm1UOGJTVkplcGdXcnJ5eA==");
             driver.findElementById(loginButtonId).click();
             
         }
 
+	@org.junit.Test
+    public void showPasswordTest() throws Exception {
+    	
+    	
+     
+        driver.findElementById(mailButtonId).sendKeys("user1@example.com");
+        driver.findElementById(passwordButtonId).sendKeys("cGFzc3dvcmQ=");
+        driver.findElementById(showPasswordId).click();
+        driver.findElementById(passwordButtonId).getText().equals("cGFzc3dvcmQ=");
+        
+    }
 }
